a652d3ab5f6ec24333b116a80972123c5dd963c9
Vivek Shinde <vivshind@publicisgroupe.net>
2023-04-14 18:54:23 +0530

    Initial Commit

---
+++ src/main/java/com/imageuploader/RestApiImageUpload/RestApiImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.imageuploader.RestApiImageUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiImageUploadApplication.class, args);
+	}
+
+}
